374e6fbe49099abf63bedd3a876adab1d52569b0
start.vaadin.com <no-reply@vaadin.com>
2022-08-30 00:42:51 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/appbeta/AppBetaView.java
@@ -0,0 +1,37 @@
+package com.example.application.views.appbeta;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import javax.annotation.security.RolesAllowed;
+
+@PageTitle("AppBeta")
+@Route(value = "AppBeta", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+public class AppBetaView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public AppBetaView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
